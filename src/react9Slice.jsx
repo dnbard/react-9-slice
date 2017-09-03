1846b309dfa78503aa9ce78b97937a17cf7e4f79
Alex Bardanov <dnbard@gmail.com>
2017-09-03 23:41:52 +0300

    some constants were introduced

--- src/react9Slice.jsx
+++ src/react9Slice.jsx
@@ -1,61 +1,63 @@
 import React from 'react';
 
 export default function React9Slice({ image, border=8, width=128, height=128, children, imageSize, style }){
-    const styleBase = {
+    const BASE_STYLE = {
         width: border,
         height: border,
         display: 'inline-flex',
         backgroundImage: image ? `url(${image})`: undefined
     };
 
-    let scaleX = width / (imageSize - border * 2);
-    // if (scaleX < 1) { scaleX = 1; }
-
-    let scaleY = height / (imageSize - border * 2);
-    // if (scaleY < 1) { scaleY = 1; }
+    const IMAGE_SIZE_MINUS_BORDER = imageSize - border * 2;
+    const IMAGE_SIZE_AND_BORDER = imageSize + border;
+    const BORDER_AND_WIDTH = border + width;
+    const MINUS_BORDER = -border;
+    const SCALE_X = width / (IMAGE_SIZE_MINUS_BORDER);
+    const SCALE_Y = height / (IMAGE_SIZE_MINUS_BORDER);
+    
 
     return <div className="r9s" style={{ lineHeight: 0, width: width + border * 2, height: height + border * 2, position: 'relative' }}>
         <div style={{ position: 'relative' }}>
-            <div className="r9s-nw" style={ Object.assign({}, styleBase) }></div>
-            <div className="r9s-n" style={ Object.assign({}, styleBase, {
-                width: imageSize - border * 2,
+            <div className="r9s-nw" style={ Object.assign({}, BASE_STYLE) }></div>
+            <div className="r9s-n" style={ Object.assign({}, BASE_STYLE, {
+                width: IMAGE_SIZE_MINUS_BORDER,
                 transformOrigin: 'left',
-                transform: `scaleX(${scaleX})`,
-                backgroundPositionX: -border
+                transform: `scaleX(${SCALE_X})`,
+                backgroundPositionX: MINUS_BORDER
             }) }></div>
-            <div className="r9s-ne" style={ Object.assign({}, styleBase, {
-                backgroundPositionX: imageSize + border,
+            <div className="r9s-ne" style={ Object.assign({}, BASE_STYLE, {
+                backgroundPositionX: IMAGE_SIZE_AND_BORDER,
                 position: 'absolute',
                 top: 0,
-                left: border + width
+                left: BORDER_AND_WIDTH
             }) }></div>
         </div>
         <div style={{ position: 'relative', height, width }}>
-            <div className="r9s-w" style={ Object.assign({}, styleBase, {
-                height: imageSize - border * 2,
-                backgroundPositionY: -border,
+            <div className="r9s-w" style={ Object.assign({}, BASE_STYLE, {
+                height: IMAGE_SIZE_MINUS_BORDER,
+                backgroundPositionY: MINUS_BORDER,
                 transformOrigin: 'left top',
-                transform: `scaleY(${scaleY})`
+                transform: `scaleY(${SCALE_Y})`
             }) }></div>
-            <div className="r9s-c" style={ Object.assign({}, styleBase, {
-                width: imageSize - border * 2,
-                height: imageSize - border * 2,
+            <div className="r9s-c" style={ Object.assign({}, BASE_STYLE, {
+                width: IMAGE_SIZE_MINUS_BORDER,
+                height: IMAGE_SIZE_MINUS_BORDER,
                 lineHeight: 1,
                 verticalAlign: 'top',
-                transform: `scaleX(${scaleX}) scaleY(${scaleY})`,
+                transform: `scaleX(${SCALE_X}) scaleY(${SCALE_Y})`,
                 transformOrigin: 'left top',
-                backgroundPositionX: -border,
-                backgroundPositionY: -border
+                backgroundPositionX: MINUS_BORDER,
+                backgroundPositionY: MINUS_BORDER
             }) }></div>
-            <div className="r9s-e" style={ Object.assign({}, styleBase, {
-                height: imageSize - border * 2,
-                backgroundPositionX: imageSize + border,
-                backgroundPositionY: -border,
+            <div className="r9s-e" style={ Object.assign({}, BASE_STYLE, {
+                height: IMAGE_SIZE_MINUS_BORDER,
+                backgroundPositionX: IMAGE_SIZE_AND_BORDER,
+                backgroundPositionY: MINUS_BORDER,
                 position: 'absolute',
                 top: 0,
-                left: border + width,
+                left: BORDER_AND_WIDTH,
                 transformOrigin: 'left top',
-                transform: `scaleY(${scaleY})`
+                transform: `scaleY(${SCALE_Y})`
             }) }></div>
             <div className="r9-content" style={ Object.assign({}, {
                 position: 'absolute',
@@ -67,25 +69,27 @@ export default function React9Slice({ image, border=8, width=128, height=128, ch
                 zIndex: 10,
                 padding: '10px 4px',
                 boxSizing: 'border-box'
-            }, style)}>{ children }</div>
+            }, style)}>
+                { children }
+            </div>
         </div>
         <div style={{ position: 'absolute', top: height + border }}>
-            <div className="r9s-sw" style={ Object.assign({}, styleBase, {
-                backgroundPositionY: imageSize + border
+            <div className="r9s-sw" style={ Object.assign({}, BASE_STYLE, {
+                backgroundPositionY: IMAGE_SIZE_AND_BORDER
             }) }></div>
-            <div className="r9s-s" style={ Object.assign({}, styleBase, {
-                width: imageSize - border * 2,
+            <div className="r9s-s" style={ Object.assign({}, BASE_STYLE, {
+                width: IMAGE_SIZE_MINUS_BORDER,
                 transformOrigin: 'left',
-                transform: `scaleX(${scaleX})`,
-                backgroundPositionX: -border,
-                backgroundPositionY: imageSize + border
+                transform: `scaleX(${SCALE_X})`,
+                backgroundPositionX: MINUS_BORDER,
+                backgroundPositionY: IMAGE_SIZE_AND_BORDER
             }) }></div>
-            <div className="r9s-se" style={ Object.assign({}, styleBase, {
-                backgroundPositionX: imageSize + border,
-                backgroundPositionY: imageSize + border,
+            <div className="r9s-se" style={ Object.assign({}, BASE_STYLE, {
+                backgroundPositionX: IMAGE_SIZE_AND_BORDER,
+                backgroundPositionY: IMAGE_SIZE_AND_BORDER,
                 position: 'absolute',
                 top: 0,
-                left: border + width
+                left: BORDER_AND_WIDTH
             }) }></div>
         </div>
     </div>;
