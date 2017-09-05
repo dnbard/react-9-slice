89567cb8a5e273f5766ee6bf4b147c62e2e72337
Alex Bardanov <dnbard@gmail.com>
2017-09-05 22:54:37 +0300

    main page with some code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Slider from 'antd/lib/slider';
+import 'antd/lib/slider/style';
+
 import React9Slice from './src/react9Slice.jsx';
 
 class Main extends Component{
@@ -14,12 +17,40 @@ class Main extends Component{
     }
 
     render(){
-        return <div>
+        return <div style={{ margin: '16px 64px' }}>
             <React9Slice width={ this.state.width } height={ this.state.height } border={ 85 } image="./src/9slice.png" imageSize={{ x: 516, y: 423 }} style={{
                 color: 'white'
             }}>HELLO WORLD</React9Slice>
-            <div style={{ marginTop: 16 }}>Width: <input value={ this.state.width } onChange={(e) => this.setState({ width: parseInt(e.target.value) }) } /></div>
-            <div>Height: <input value={ this.state.height } onChange={(e) => this.setState({ height: parseInt(e.target.value) }) } /></div>
+            <div>
+                <pre style={{ margin: 16, background: 'rgba(0, 0, 0, 0.59)', color: 'white', borderRadius: 4, display: 'inline-flex', padding: '8px 16px', flexDirection: 'column' }}>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                        { "<React9Slice width=" }
+                        <Slider style={{ width: 150, margin: '0 8px' }}
+                                onChange={ v => this.setState({ width: v }) }
+                                value={ this.state.width }
+                                min={ 250 }
+                                max={ 600 } />
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', marginLeft: 84 }}>
+                        height=
+                        <Slider style={{ width: 150, margin: '0 8px' }}
+                                onChange={ v => this.setState({ height: v }) }
+                                value={ this.state.height }
+                                min={ 100 }
+                                max={ 400 } />
+                        
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', marginLeft: 84 }}>
+                        image="/images/some-image.png"{ ">" }
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', marginLeft: 42 }}>
+                        HELLO WORLD
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        { "</React9Slice>" }
+                    </div>
+                </pre>
+            </div>
         </div>;
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.less$/, loaders: ['style-loader', 'css-loader', 'less-loader'] }
         ]
     },
     plugins: [ HtmlWebpackPluginConfig ]
